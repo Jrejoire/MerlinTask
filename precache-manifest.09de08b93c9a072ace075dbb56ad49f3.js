f72d28371c5d89fcfb82d18e3a563c59cf686eb5
root <jrejoire@hotmail.fr>
2019-12-02 11:54:57 +0100

    Updates

---
+++ precache-manifest.09de08b93c9a072ace075dbb56ad49f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "959f837de73487b6127d001ca0e52165",
+    "url": "/MerlinTask/index.html"
+  },
+  {
+    "revision": "1f7b3d6be88d83ba576d",
+    "url": "/MerlinTask/static/css/main.3ec18672.chunk.css"
+  },
+  {
+    "revision": "7a5d859296030e50b0fa",
+    "url": "/MerlinTask/static/js/2.4615fdb2.chunk.js"
+  },
+  {
+    "revision": "1f7b3d6be88d83ba576d",
+    "url": "/MerlinTask/static/js/main.cdfced9c.chunk.js"
+  },
+  {
+    "revision": "c0ccf9555c7784e71d93",
+    "url": "/MerlinTask/static/js/runtime-main.b3915e8f.js"
+  }
+]);
