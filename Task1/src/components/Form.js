46a1019ffdc879cade746d9790c657fda6f35c15
jrej <jrejoire@hotmail.fr>
2019-12-02 11:38:58 +0100

    layout fix

--- Task1/src/components/Form.js
+++ Task1/src/components/Form.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import TextField from '@material-ui/core/TextField';
-import Grid from '@material-ui/core/Grid';
+import { FormControl, Grid, TextField } from '@material-ui/core';
 import { withStyles } from "@material-ui/core/styles";
 import './Form.css';
 
@@ -10,63 +9,76 @@ const styles = theme => ({
     }
 });
 
-
 const Form = withStyles(styles)(props => {
     const { classes } = props;
     return (
-        <form className="main">
-            <h2>Register</h2>
-            <TextField
-                className="text-field"
-                id="full-name"
-                label="Full Name"
-                defaultValue=" "
-                margin="normal"
-                variant="outlined"
-                InputProps={{
-                    className: classes.input
-                }}
-                InputLabelProps={{
-                    shrink: true
-                }}
-            />
+        <Grid 
+            container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justify="center"
+        >
+            <FormControl className="main">
+                <h2>Register</h2>
 
-            <TextField
-                className="text-field"
-                id="email"
-                type="mail"
-                label="E-mail"
-                defaultValue=" "
-                margin="normal"
-                variant="outlined"
-                InputProps={{
-                    className: classes.input
-                }}
-                InputLabelProps={{
-                    shrink: true
-                }}
-            />
+                <TextField
+                    className="text-field"
+                    id="full-name"
+                    label="Full Name"
+                    defaultValue=" "
+                    margin="normal"
+                    variant="outlined"
+                    InputProps={{
+                        className: classes.input
+                    }}
+                    InputLabelProps={{
+                        shrink: true
+                    }}
+                />
+                <TextField
+                    className="text-field"
+                    id="email"
+                    type="mail"
+                    label="E-mail"
+                    defaultValue=" "
+                    margin="normal"
+                    variant="outlined"
+                    InputProps={{
+                        className: classes.input
+                    }}
+                    InputLabelProps={{
+                        shrink: true
+                    }}
+                />
+                <TextField
+                    className="text-field"
+                    id="password"
+                    label="Password"
+                    defaultValue=" "
+                    autoComplete="current-password"
+                    margin="normal"
+                    variant="outlined"
+                    InputProps={{
+                        className: classes.input
+                    }}
+                    InputLabelProps={{
+                        shrink: true
+                    }}
+                />
+                <div className="button-container">
+                    <button className="register-button" type="submit" value="submit">
+                        register
+                    </button>
+                </div>
+            </FormControl>
+            <a className="oauth-container" href="/google-oauth">
+                <img width="15px" alt="google-logo" className="google-logo"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" />
+                <p className="google-signin"> Sign in with Google </p>
+            </a>
 
-            <TextField
-                className="text-field"
-                id="password"
-                label="Password"
-                defaultValue=" "
-                autoComplete="current-password"
-                margin="normal"
-                variant="outlined"
-                InputProps={{
-                    className: classes.input
-                }}
-                InputLabelProps={{
-                    shrink: true
-                }}
-            />
-
-            <button className="register-button" type="submit" value="submit">
-                Register
-                </button>
-        </form>
+        </Grid>
     );
 });
 
--- Task1/src/components/Login.js
+++ Task1/src/components/Login.js
@@ -6,18 +6,15 @@ function Login() {
   return (
     <div className="login">
       <div className="left-side">
-        <h1>REDEFINING MOBILE ADVERTISING</h1>
-        <p>Please use this form to register.</p>
-        <p>If you are a member, please <a href='#login'>login</a>.</p>
+        <div className='left-container'>
+          <h1 className="title">Redefining mobile advertising</h1>
+          <p>Please use this form to register.</p>
+          <p>If you are a member, please <a href='/login'>login</a>.</p>
+        </div>
       </div>
       <div className="right-side">
-        <img src="asset3.png" alt="logo" />
+        <img src="asset3.png" alt="logo" className="logo" />
         <Form className="form" />
-        <a className="oauth-container" href="/users/google-oauth/">
-          <img width="15px" alt="google-logo" className="google-logo"
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" />
-          <p className="google-signin"> Sign in with Google </p>
-        </a>
       </div>
     </div>
   );
